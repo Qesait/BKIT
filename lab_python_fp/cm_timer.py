91bb2c83338104c0ecdcd656289a06a0253e31ba
Qesait <kuzva2003@gmail.com>
2022-10-08 18:10:47 +0300

    cm_timer.py

---
+++ lab_python_fp/cm_timer.py
@@ -0,0 +1,27 @@
+from time import perf_counter, sleep
+from contextlib import contextmanager
+
+
+class cm_timer_1:
+    def __init__(self):
+        self.start = None
+
+    def __enter__(self):
+        self.start = perf_counter()
+
+    def __exit__(self, exp_type, exp_value, traceback):
+        print('time: {:.1f}'.format(perf_counter() - self.start))
+
+
+@contextmanager
+def cm_timer_2():
+    start = perf_counter()
+    yield
+    print('time: {:.1f}'.format(perf_counter() - start))
+
+
+if __name__ == '__main__':
+    with cm_timer_1():
+        sleep(1.5)
+    with cm_timer_2():
+        sleep(1.5)
