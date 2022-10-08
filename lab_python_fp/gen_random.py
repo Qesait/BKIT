c7904a5c33813e889c957c921e6df2279716c8de
Qesait <kuzva2003@gmail.com>
2022-10-08 16:56:34 +0300

    gen_random.py

---
+++ lab_python_fp/gen_random.py
@@ -0,0 +1,11 @@
+from random import randint
+
+
+def gen_random(num_count, begin, end):
+    for i in range(num_count):
+        yield randint(begin, end)
+
+
+if __name__ == '__main__':
+    print(list(gen_random(5, 1, 3)))
+    print(list(gen_random(5, -5, 5)))
