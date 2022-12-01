fa822d2a6437c1c1528d9eb8e1d5d0f7a29b6af7
Qesait <kuzva2003@gmail.com>
2022-12-01 10:31:54 +0300

    Дополнение

--- service.py
+++ service.py
@@ -7,7 +7,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return '<h1>Returning the lucky numbers!</h1><a href="https://oeis.org/A000959">Последовательность на OEIS</a>'
+    html = """<h1>Returning the lucky numbers!</h1>
+              <a href="https://oeis.org/A000959">The sequence in the OEIS<br></a>
+              <a href="https://ru.wikipedia.org/wiki/Счастливое_число_(lucky_number)">Wikipedia</a>"""
+    return html
 
 
 @app.route('/num/<int:cnt>')
